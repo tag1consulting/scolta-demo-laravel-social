e9d3291600808ab6318cb66ef8122492e3fa8457
Jeremy Andrews <jeremy@tag1consulting.com>
2026-04-30 15:34:20 +0200

    Bump scolta to 0.3.7, remove asset-copy workaround

--- .ddev/post-start.sh
+++ .ddev/post-start.sh
@@ -15,12 +15,3 @@ fi
 
 ddev exec php artisan config:cache
 ddev exec php artisan route:cache
-
-# Workaround for scolta v0.3.5 asset bundling bug (remove after v0.3.6)
-# See: https://github.com/tag1consulting/scolta-laravel/issues/XX
-if [ -f vendor/tag1/scolta-php/assets/js/scolta.js ]; then
-    ddev exec mkdir -p vendor/tag1/scolta-laravel/public/js
-    ddev exec mkdir -p vendor/tag1/scolta-laravel/public/css
-    ddev exec cp vendor/tag1/scolta-php/assets/js/scolta.js vendor/tag1/scolta-laravel/public/js/
-    ddev exec cp vendor/tag1/scolta-php/assets/css/scolta.css vendor/tag1/scolta-laravel/public/css/ 2>/dev/null || true
-fi
